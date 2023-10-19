d550b054edf48bb74ade4bb257ca32f137cd3bee
Lancelot the Brave <l.brave@spamalot.uk>
2023-10-19 10:29:21 +0100

    added a nested list and dictionary to represent the relationship between people

--- group.py
+++ group.py
@@ -2,4 +2,25 @@
 
 # Your code to go here...
 
-my_group =
+
+def print_connection():
+    print()
+
+
+class People:
+    def __int__(self, name, age, job, connection):
+        self.name = name
+        self.age = age
+        self.job = job
+        self.connection = connection
+
+
+my_group = [{'name': 'Jill', 'age': 8, 'job': 'biologist',
+             'relationship': {'partner':['John']}},
+            {'name': 'Zalika', 'age':28, 'job': 'artist',
+             'relationship': {}},
+            {'name': 'John', 'age': 27, 'job': 'writer',
+             'relationship': {}},
+            {'name': 'Nash', 'age': 34, 'job': 'chef',
+             'relationship': {}}
+            ]
