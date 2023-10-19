abce5f686e4ff9c3e05d4c3712964b55f236e323
superheroa6 <zczqjy2@ucl.ac.uk>
2023-10-19 11:04:29 +0100

    Olivia copy

--- group.py
+++ group.py
@@ -2,4 +2,12 @@
 
 # Your code to go here...
 
-my_group =
+my_grouop ={'Jill':{'age': 26, 'job': 'biologist',
+                    'relationships': {'friend': ['Zalika'], 'partner':['John']}},
+            'Zalika': {'age': 28, 'job': 'artist',
+                     'relationships': {'friend': ['Jill'], 'landlord': ['Nash']}},
+            'John': {'age': 27, 'job': 'writer',
+                     'relationships': {'partner': ['Jill']}},
+            'Nash': {'age': 34, 'job': 'chef',
+                     'relationships': {'uncle': ['John'], 'employee': ['Zalika']}}
+            }
